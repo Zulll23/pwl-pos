d1057cb9342f17203f23401d635916b3730d57c4
Zulll23 <mochzulfaakbarmaulana@gmail.com>
2024-08-14 07:38:27 +0700

    Pertemuan 22

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,30 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
-
-     //   $data=
-       // [
-         //   'username'=>'dika',
-           // 'nama'=>'dika',
-            //'password'=>Hash::make('12345'),
-            //'user_id'=>4,
-            //'level_id'=>1
+    public function index()
+    {
+        // $data=
+        //[
+          //  'username'=>'dika',
+          //  'nama'=>'dika',
+          //  'password'=>Hash::make('12345'),
+          //  'user_id'=>4,
+          //  'level_id'=>1
         //];
+        //UserModel::insert($data);
+        
 
-        $data=[
-            'nama'=>'antok',
+        $data = 
+        [
+            'nama'=>'bartok',
         ];
+        UserModel::where('username','dika')->update($data);
+
         UserModel::where('username','dika')->delete();
 
-        $user = UserModel::all();
-        return view('user',['data' => $user]);
+
+
+        //$user = UserModel::all();
+        //return view('user',['data'=>$user]);
+
+        $user = UserModel::with('level')->get();
+        return view('user',['data' =>$user]);
     }
 }
