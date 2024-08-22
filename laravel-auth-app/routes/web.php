af8d7d4e8e765b8d8f1214fd3a9445f12b8ad1b5
Zulll23 <mochzulfaakbarmaulana@gmail.com>
2024-08-22 10:32:05 +0700

    Praktikum 1 Pertemuan 28

--- laravel-auth-app/routes/web.php
+++ laravel-auth-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
+Route::resource('articles',ArticleController::class);
 
 /*
 |--------------------------------------------------------------------------
